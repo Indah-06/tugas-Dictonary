5324cee79a254c0b9e50b9c47ff27a06115cef0d
Indah Nurhayati <iindahnurhayatii@gmail.com>
2025-04-17 08:25:44 -0400

    Add files via upload

---
+++ hasiltugasdictonary.py
@@ -0,0 +1,10 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:\Users\ASUS\OneDrive\Documents\tugasdictonary.py =========
+masukkan kata yang ingin dicari artinya:pisang
+Arti dari'[pisang]'adalah:[pisang[buah berwarna kuning]}
+masukkan kata yang ingin dicari artinya:stroberi
+Arti dari'[stroberi]'adalah:[stroberi[kaya akan vitamin C]}
+masukkan kata yang ingin dicari artinya:alpukat
+Arti dari'[alpukat]'adalah:[alpukat[kaya akan asam lemak sehat]}
